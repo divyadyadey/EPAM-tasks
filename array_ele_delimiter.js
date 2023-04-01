741931840e4467777fba4798e83df98411e8cf4e
divyadyadey <divyadyadey@gmail.com>
2023-04-01 11:55:56 +0530

    day1 day2

---
+++ array_ele_delimiter.js
@@ -0,0 +1,15 @@
+//Printing Array elements with Comma delimiters
+
+/*DESCRIPTION:
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"*/
+
+function delimiter(a){
+    return a.join(',')
+    
+}
---
+++ basic_maths.js
@@ -0,0 +1,17 @@
+/*Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output*/
+
+function basic_math_op(op,a,b){
+    if(op==='+')
+        return a+b;
+    else if(op==='-')
+        return a-b;
+    else if(op==='*')
+        return a*b;
+    else if(op==='/')
+        return a/b;
+}
